eb14851ff02e85d44be9622fae61896bac79067e
Stephen Bowen <bowensteven17@gmail.com>
2024-03-19 20:25:34 +0300

    Completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 =31;
+const num2 =2;
+const num3 =34;
+const num4 =5;
+var multiply = num1*num2;
+var random = Math.floor(Math.random() * 10) + 1;;
+var mod = num3%num4;
+var max = Math.max(1, 5, 11, 16, 18, 20);
